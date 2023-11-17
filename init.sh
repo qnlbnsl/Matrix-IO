37f8238f26d7acd29e62b81fdadf17f1c04d2c7b
Kunal <qnlbnsl@gmail.com>
2023-11-17 16:36:54 -0500

    Update submodule initialization

--- init.sh
+++ init.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-mkdir -p build src
-cd build || exit
-mkdir -p matrix-creator-hal matrix-creator-init matrixio-kernel-modules matrix-lite-py matrix-creator-openocd xc3sprog
-cd ../src || exit
-
 echo "Downloading source code..."
 
-git clone git@github.com:qnlbnsl/matrix-creator-hal.git
-git clone git@github.com:qnlbnsl/matrix-creator-init.git
-git clone git@github.com:qnlbnsl/matrixio-kernel-modules.git
-git clone git@github.com:qnlbnsl/matrix-lite-py.git
-git clone git@github.com:qnlbnsl/matrix-creator-openocd.git
-git clone git@github.com:qnlbnsl/xc3sprog.git
-# git clone https://github.com/msteveb/jimtcl.git
+git submodule init && git submodule update --recursive
